595edf1a130d387d20420f12089f3a4766d9e01a
Benjamin <erdle.benjamin@gmail.com>
2026-03-10 22:51:36 +0100

    fixed copy paste mistake

--- modules/finalmouse.nix
+++ modules/finalmouse.nix
@@ -7,7 +7,7 @@
 {
   options.hardware.finalmouse.enable = lib.mkEnableOption "support for finalmouse devices";
 
-  config = lib.mkIf config.hardware.wooting.enable {
+  config = lib.mkIf config.hardware.finalmouse.enable {
     environment.systemPackages = [ pkgs.xpanel ];
     services.udev.packages = [ pkgs.finalmouse-udev-rules ];
   };
